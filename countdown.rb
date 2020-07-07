0439f3ae4385be005d49358fe6823b65bcc7e52a
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 00:46:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,13 @@ def countdown(x)
   end
   "HAPPY NEW YEAR!"
 end
+
+
+def countdown(x)
+  counter = x
+  while counter > 0
+    puts "#{x} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
